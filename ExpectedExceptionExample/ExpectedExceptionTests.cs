e56ccbc9ea077d5f9f670209016791cb50fddcc5
nrosanta <nrosanta@gmail.com>
2019-11-26 12:22:33 -0500

    Initial check-in of module NUnitDemoApp

---
+++ ExpectedExceptionExample/ExpectedExceptionTests.cs
@@ -0,0 +1,17 @@
+using System;
+using NUnit.Framework;
+
+namespace ExpectedExceptionExample
+{
+    [TestFixture]
+    public class ExpectedExceptionTests
+    {
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void HandlesArgumentExceptionAsType()
+        {
+            throw new ArgumentException();
+            //throw new ArgumentNullException();
+        }
+    }
+}
